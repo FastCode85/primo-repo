9e1ac9374e8dc685ca1b58525c0a88ce943fc092
Marco <marcomarco85@hotmail.it>
2026-02-02 11:31:16 +0100

    metodi di console

--- esercitazione-01/Program.cs
+++ esercitazione-01/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Questo è un commento
+
+/*
+Commento multilinea
+*/
 Console.WriteLine("Hello, World!");
+//Console è una classe predefinita di c# che mette a disposizione diversi metodi
+//WriteLine è il metodo della classe Console che stampa a monitor
+//Generalmente ogni metodo ha le parentesi all' interno delle quali si specificano i parametri o gli argomento
+
+Console.Write("Inserisci il tuo nome: ");
+
+
+string nome=Console.ReadLine(); //Acquisire l'input dell' utente in una variabile
+
+//stampo il nome acquisito
+Console.WriteLine("Nome acquisito: "+nome+"!");
+
+Console.Write("Inserisci il cognome: ");
+string cognome=Console.ReadLine(); //Acquisire l'input dell' utente in una variabile
+//stampa tramite interpolazione di stringa
+Console.WriteLine($"Ciao, {nome} {cognome}!");
