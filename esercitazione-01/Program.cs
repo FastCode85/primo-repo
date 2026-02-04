2555bb6c9866fe77f3211b99d2d73c6b03486401
Marco <marcomarco85@hotmail.it>
2026-02-04 09:44:27 +0100

    esercitazione 1 completata

--- esercitazione-01/Program.cs
+++ esercitazione-01/Program.cs
@@ -3,20 +3,17 @@
 /*
 Commento multilinea
 */
-Console.WriteLine("Hello, World!");
+
 //Console è una classe predefinita di c# che mette a disposizione diversi metodi
 //WriteLine è il metodo della classe Console che stampa a monitor
 //Generalmente ogni metodo ha le parentesi all' interno delle quali si specificano i parametri o gli argomento
 
-Console.Write("Inserisci il tuo nome: ");
+Console.Write("Inserisci il tuo nome: "); //stampa per input
+string nome=Console.ReadLine(); //Acquisire l'input dell' utente in una variabile nome
 
 
-string nome=Console.ReadLine(); //Acquisire l'input dell' utente in una variabile
+Console.Write("Inserisci il cognome: "); //stampa per input
+string cognome=Console.ReadLine(); //Acquisire l'input dell' utente in una variabile cognome
 
-//stampo il nome acquisito
-Console.WriteLine("Nome acquisito: "+nome+"!");
-
-Console.Write("Inserisci il cognome: ");
-string cognome=Console.ReadLine(); //Acquisire l'input dell' utente in una variabile
 //stampa tramite interpolazione di stringa
-Console.WriteLine($"Ciao, {nome} {cognome}!");
+Console.WriteLine($"Ciao, {nome} {cognome}, benvenuto!");
