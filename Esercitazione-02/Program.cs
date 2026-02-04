8d0aa49bf407e52b8743bb01de4bcd910cbe016d
Marco <marcomarco85@hotmail.it>
2026-02-04 12:15:52 +0100

    Esercitazione 2 errore risolto

--- Esercitazione-02/Program.cs
+++ Esercitazione-02/Program.cs
@@ -8,12 +8,3 @@ List<string> listaStringhe=new List<string> {"Nome1","Nome2","Nome3"}; //crea un
 listaStringhe.Add("Nome4"); //aggiungo un nome alla lista
 Console.WriteLine($"Stampa ultimo nome: {listaStringhe[3]}");//stampa ultimo nome
 
-Dictionary<int,string> dizionario=new Dictionary<int,string>//creo e inizializzo il dizionario
-{
-    {1,"Valore 1"},
-    {2,"Valore 2"},
-    {3,"Valore 3"}
-
-};
-
-Console.WriteLine($"Valore due del dizionario: {dizionario[2]}");//stampa il valore con chiave 2
--- Esercitazione-03/Program.cs
+++ Esercitazione-03/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Dictionary<int,string> dizionario=new Dictionary<int,string>//creo e inizializzo il dizionario
+{
+    {1,"Valore 1"},
+    {2,"Valore 2"},
+    {3,"Valore 3"}
+
+};
+Console.WriteLine($"Valore due del dizionario: {dizionario[2]}");//stampa il valore con chiave 2
+
+Dictionary<string,int> dizionario2=new Dictionary<string,int>();
+dizionario2.Add("chiave1",1);
+dizionario2.Add("chiave2",2);
+dizionario2.Add("chiave3",3);
+Console.WriteLine($"Valore due del dizionario: {dizionario2["chiave2"]}");//stampa il valore con chiave 2
