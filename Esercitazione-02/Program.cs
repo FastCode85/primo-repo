db814c7b70ef2f1432815108e95c761dba279adb
Marco <marcomarco85@hotmail.it>
2026-02-04 11:30:11 +0100

    Esercitazione2 completata

---
+++ Esercitazione-02/Program.cs
@@ -0,0 +1,9 @@
+﻿int[] arrayNumeri= {5,6,3,5,8};  //dichiarazione e inizializzazione
+Console.WriteLine($"Ecco il primo numero dell'array: {arrayNumeri[0]}");  //stampa del primo numero
+
+List<int> listaNumeri=new List<int> {10,20,30};
+Console.WriteLine($"valore zero della lista: {listaNumeri[0]}");
+
+List<string> listaStringhe=new List<string> {"Nome1","Nome2","Nome3"}; //cre una lista di stringhe
+listaStringhe.Add("Nome4"); //aggiungo un nome alla lista
+Console.WriteLine($"Stampa ultimo nome: {listaStringhe[3]}");//stampa ultimo nome
