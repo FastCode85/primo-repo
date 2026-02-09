ae511e4e30239cc5dcf67db02b1c1b5a8f845f08
Marco <marcomarco85@hotmail.it>
2026-02-09 12:53:59 +0100

    Esercitazione 6 parte 3

--- Esercitazione-05/Program.cs
+++ Esercitazione-05/Program.cs
@@ -1,93 +1,21 @@
-﻿for(int i = 0; i <= 10; i++)
+﻿Random random=new Random();  //creazione di un oggetto random
+int var=1;
+int nVolte=0;
+List<int> listaNumeri=new List<int>();
+while(var==1)
 {
-    Console.WriteLine($"numero {i}");
+    if(nVolte>=5)
+        break;
+    int valore=random.Next(1,7);
+    if(valore>=5)
+        listaNumeri.Add(valore);
+    Console.WriteLine($"Il lancio ha dato risultato {valore}. Premi 1 per rilanciare");
+    var=int.Parse(Console.ReadLine());
+    nVolte++;
+
 }
 
-List<string> nomi=new List<string> {"Nome1","Nome2","Nome3"};
-foreach(var variabile in nomi)
-{   
-    Console.WriteLine(variabile);
-
-}
-
-Dictionary<int,string> dizionario=new Dictionary<int,string>
+foreach(var variabile in listaNumeri)
 {
-    {1,"uno"},
-    {2,"due"},
-    {3,"tre"}
-};
-
-foreach(var kvp in dizionario)
-{
-    Console.WriteLine($"Chiave: {kvp.Key}, Valore: {kvp.Value}");
-}
-
-int n=1;
-while (n <= 10)
-{
-    Console.WriteLine($"Numero {n}");
-    n++;
-}
-
-int k=1;
-do
-{
-    Console.WriteLine($"Numero {k}");
-    k++;
-} while (k <= 10);
-
-for(int z = 0; z < 100; z++)
-{
-    Console.WriteLine($"Ciclo per numero: {z}");
-    if(z%3==0 && z%5==0)
-        Console.WriteLine("Fizzbuzz");
-    else if(z%3==0 && z%5!=0)
-        Console.WriteLine("Fizz");
-    else if(z%3!=0 && z%5==0)
-        Console.WriteLine("Buzz");
-    else
-        Console.WriteLine("Numero non divisibile per 3 e per 5");
-}
-
-int[] arrayDiNumeri={345,3563,34534};
-foreach(var variabile in arrayDiNumeri)
-{
-    Console.WriteLine($"Ciclo per numero {variabile}");
-    if(variabile%3==0 && variabile%5==0)
-        Console.WriteLine("Fizzbuzz");
-    else if(variabile%3==0 && variabile%5!=0)
-        Console.WriteLine("Fizz");
-    else if(variabile%3!=0 && variabile%5==0)
-        Console.WriteLine("Buzz");
-    else
-        Console.WriteLine("Numero non divisibile per 3 e per 5");
-}
-
-for(int i = 0; i < 5; i++)
-{
-    Console.WriteLine("Inserisci un nuovo numero");
-    string numeroStringa = Console.ReadLine();
-    if(int.TryParse(numeroStringa, out int risultato))
-        Console.WriteLine($"Il numero inserito è {risultato}");
-    else
-        Console.WriteLine("Il valore inserito non è un numero");
-}
-
-List<string> listaNumeriStr=new List<string>(); //dichiarazione di una lista
-Console.WriteLine("Inserimento di una lista di numeri. Inserisci un numero");
-string stringaInput=Console.ReadLine(); //lettura del valore da console
-while(stringaInput!="stop")//ciclo di inserimento numeri
-{
-    listaNumeriStr.Add(stringaInput);//aggiunge la stringa alla lista
-    Console.WriteLine("Inserisci un numero");
-    stringaInput=Console.ReadLine();
-}
-
-List<int> listaNumeriInteri=new List<int>();
-foreach(var variabile in listaNumeriStr)
-{
-    listaNumeriInteri.Add(int.Parse(variabile));//aggiunge variabile alla lista di interi
-    
-    Console.WriteLine($"Stampo numero {variabile}");
-
+    Console.WriteLine($"Stampa di un numero {variabile}");
 }
