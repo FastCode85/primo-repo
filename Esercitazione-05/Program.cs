a0b4418f24dd3df291cc0537d2f5bb30dc4b01f8
Marco <marcomarco85@hotmail.it>
2026-02-10 10:34:27 +0100

    Esercizio dato completato

--- Esercitazione-05/Program.cs
+++ Esercitazione-05/Program.cs
@@ -1,21 +1,43 @@
-﻿Random random=new Random();  //creazione di un oggetto random
-int var=1;
+﻿/**
+chiede all' utente di lanciare un dado per 5 volte (premendo 1)
+dopo ogni lancio chiede all utente se vuole tenere il risultato del lancio
+aggiunge ad una lista i risultati dei lanci tenuti
+stampa la lista deu risultati tenuti e la somma dei valori dei dadi
+*/
+
 int nVolte=0;
-List<int> listaNumeri=new List<int>();
-while(var==1)
+Random random=new Random();
+List<int> listaLanci=new List<int>();
+while (true)
 {
+    Console.WriteLine("Inserisci 1 per lanciare il dado");
+    int valore=int.Parse(Console.ReadLine());
+    
+    if(valore==1)
+    {
+        nVolte++;
+        int valoreRandom=random.Next(1,7);
+        Console.WriteLine($"E' uscito il numero {valoreRandom}. Inserisci 1 per tenere il numero");
+        int valoreTenere=int.Parse(Console.ReadLine());
+        if(valoreTenere==1)
+        {
+            listaLanci.Add(valoreRandom);
+        }
+    }
+    else
+    {
+        Console.WriteLine("Input non valido, riprova");
+    }
     if(nVolte>=5)
         break;
-    int valore=random.Next(1,7);
-    if(valore>=5)
-        listaNumeri.Add(valore);
-    Console.WriteLine($"Il lancio ha dato risultato {valore}. Premi 1 per rilanciare");
-    var=int.Parse(Console.ReadLine());
-    nVolte++;
-
 }
 
-foreach(var variabile in listaNumeri)
+int somma=0;
+int counter=0;
+foreach (var lancio in listaLanci)
 {
-    Console.WriteLine($"Stampa di un numero {variabile}");
+    somma+=lancio;
+    Console.WriteLine($"Valore numero {counter}: {lancio}");
 }
+
+Console.WriteLine($"La somma dei numeri tenuti è {somma}");
