72d5eaf7d2dd16ad568600e96d70a66017ae235a
Marco <marcomarco85@hotmail.it>
2026-02-12 12:52:41 +0100

    Esercitazione prodotti

--- Esercitazione-05/Program.cs
+++ Esercitazione-05/Program.cs
@@ -1,34 +1,139 @@
 ﻿/*
-Programma che:
-- chiede all' utente di inserire nomi ad una lista finché non viene inserito 0
-- viene estratto un nome casuale e stampato a video
-- viene chiesto all' utente se vuole estrarre un altro nome casuale, se l'utente risponde "y" viene estratto e stampato 
-  un altro nome casuale, altrimenti il programma termina
-- il nome estratto deve essere rimosso dalla lista per evitare che venga estratto di nuovo
-*/
 
+- chiede all' utente di inserire il nome di un prodotto il prezzo e la quantità intervallati da una virgola
+- si ripulisce la stringa dagli spazi
+- verifichiamo che l' input sia valido ( ossia quando ha tre parti )
+- se l'input non è corretto richiede di nuovo l'input
+- ogni prodotto viene aggiunto ad una lista di stringhe con il formato "nome: prodotto - prezzo: 10.5 - quantità: 2"
+- viene chiesto ll' utente se vuole inserire un altro prodotto
+- se l' utente risponde "y" continua, altrimenti esce
+- la lista viene ordinata in ordine alfabetico e stampata a video
+ */
 
-List<string> listaNomi=new List<string>();
-string risp="1";
-//ciclo per l'inserimento dei nomi
-while(risp!="0")
+int parti;
+string risp;
+List<string> listaProdotti=new List<string>();
+
+while(true)
 {
-    Console.WriteLine("Inserisci un nome o premi 0 per uscire");
+    Console.WriteLine("Inserisci un prodotto");
     risp=Console.ReadLine();
-    if(risp!="0")
+    risp=risp.Trim();
+    string[] arrayString=risp.Split(",");
+    parti=arrayString.Length;
+    if(parti==3)
     {
-        listaNomi.Add(risp);
+        
+        bool valid=true;
+        foreach(string s in arrayString)
+        {
+            string trimmed=s.Trim();
+            if(trimmed.Length==0)
+            {
+                valid=false;
+                break;
+            }
+        }
+        if(valid)
+        {
+            string[] trimmedArray=new string[arrayString.Length];
+            int index=0;
+            foreach(string s in arrayString)
+            {
+                trimmedArray[index]=s.Trim();
+                index++;
+            }
+            listaProdotti.Add(string.Join(",",trimmedArray));
+            break;
+        }
     }
 }
 
-Random random=new Random();
-risp="y";
-while (risp == "y" && listaNomi.Count>0)
+while(true)
 {
-    int numeroRandom=random.Next(0,listaNomi.Count);
-    Console.WriteLine($"Estratto un nome casuale: {listaNomi[numeroRandom]}. Premi y per estrarre un altro nome.");
-    listaNomi.RemoveAt(numeroRandom);
-    risp=Console.ReadLine();
 
-    
+    parti=risp.Split(",").Length;
+    string[] arrayString=risp.Split(",");
+    if(parti!=3)
+    {
+        Console.WriteLine("Input non valido, reinserisci il prodotto");
+        risp=Console.ReadLine();
+        parti=arrayString.Length;
+        if(parti==3)
+        {
+            
+            bool valid=true;
+            foreach(string s in arrayString)
+            {
+                string trimmed=s.Trim();
+                if(trimmed.Length==0)
+                {
+                    valid=false;
+                    break;
+                }
+            }
+            if(valid)
+            {
+
+                string[] trimmedArray=new string[arrayString.Length];
+                int index=0;
+                foreach(string s in arrayString)
+                {
+                    trimmedArray[index]=s.Trim();
+                    Console.WriteLine($"trimmed: {trimmedArray[index]} original: {s}");
+                    index++;
+                }
+                listaProdotti.Add(string.Join(",",trimmedArray));
+                break;
+            }
+        }
+    }
+    else
+    {
+        Console.WriteLine("Premi y per aggiungere un nuovo prodotto");
+        risp=Console.ReadLine();
+        if(risp=="y")
+        {
+            Console.WriteLine("Inserisci un nuovo prodotto");
+            risp=Console.ReadLine();
+            arrayString=risp.Split(",");
+            parti=risp.Split(",").Length;
+            if(parti==3)
+            {
+
+                bool valid=true;
+                foreach(string s in arrayString)
+                {
+                    string trimmed=s.Trim();
+                    if(trimmed.Length==0)
+                    {
+                        valid=false;
+                        break;
+                    }
+                }
+                if(valid)
+                {
+
+                    string[] trimmedArray=new string[arrayString.Length];
+                    int index=0;
+                    foreach(string s in arrayString)
+                    {
+                        trimmedArray[index]=s.Trim();
+
+                        index++;
+                    }
+                    listaProdotti.Add(string.Join(",",trimmedArray));
+                    
+                }
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
 }
+
+listaProdotti.Sort();
+Console.WriteLine($"Risultato lista: {string.Join("|",listaProdotti)}");
+ 
