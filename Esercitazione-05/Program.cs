755e42d9fce4447d7dfd35dcad023d609b3c781d
Marco <marcomarco85@hotmail.it>
2026-02-16 10:43:16 +0100

    Esercitazione generatore password

--- Esercitazione-05/Program.cs
+++ Esercitazione-05/Program.cs
@@ -1,86 +1,69 @@
 ﻿/*
 
-- crea un dizionario che associa un nome ad un numero di telefono con qualche dato di default
-- permette all' utente di aggiungere un numero di telefono
-- permette all'utente di modificare il numero di telefono associato ad un nome esistente
-- permette all' utente di eliminare un nome e il suo numero di telefono dal dizionario
-- nel caso di inserimento di un nome già esistente, il programma chiede se si vuole aggiornare il numero associato
-- nel caso di modifica e cancellazione deve prima stampare la rubrica in modo da poter visualizzare i nomi e i numeri prima di effettuare l'operazione
-- nel caso di eliminazione di un nome inesistente, stampare un messaggio d'errore
+Programma che:
+- genera una password casuale di lunghezza compresa tra i 5 e 8 caratteri, che deve contenere almeno una lettera maiuscola, 
+una minuscola, un numero e un carattere speciale. La password non deve contenere spazi
+
 */
 
-Dictionary<string,string> dizionarioNumeri=new Dictionary<string,string>()
-{
-   {"marco","12345"}, 
-   {"andrea","54321"},
-   {"gianni","62254"},
-};
+int numeroCaratteri;
+int counter=0;
+Random random=new Random();
+numeroCaratteri=random.Next(5,9);
 
 
-while(true)
+bool passwordValida=false;
+Console.WriteLine($"Generazione di password di {numeroCaratteri} caratteri");
+while(!passwordValida)
 {
-    Console.Clear();
-    Console.WriteLine("Premi 1 per inserire, 2 per modificare, 3 per eliminare, 4 per visualizzare, altro per uscire");
-    string risp=Console.ReadLine().Trim();
-    Console.Clear();
-    if(risp=="1")
+    bool haMaiuscola=false;
+    bool haMinuscola=false;
+    bool haNumero=false;
+    bool haCarattereSpeciale=false; 
+    string password="";
+    counter=0;
+    while(counter<numeroCaratteri)
     {
-        Console.WriteLine("Inserisci un nome");
-        string nomeRubrica=Console.ReadLine();
-        Console.WriteLine("Inserisci un numero");
-        string numeroRubrica=Console.ReadLine();
-        dizionarioNumeri.Add(nomeRubrica, numeroRubrica);
-    }
-    else if(risp=="2")
-    {
-        Console.WriteLine("Stampa del dizionario");
-        foreach(var kvp in dizionarioNumeri)
+        char c=' ';
+        while(c==' ')
         {
-            Console.WriteLine($"nome: {kvp.Key}\tnumero: {kvp.Value}");
-        }
-        Console.WriteLine("Quale elemento vuoi modificare?");
-        string nomeDaModificare=Console.ReadLine().Trim();
-        if(dizionarioNumeri.ContainsKey(nomeDaModificare))
-        {
-            Console.WriteLine($"inserisci il nuovo numero per {nomeDaModificare}");
-            string nuovoNumero=Console.ReadLine().Trim();
-            if(dizionarioNumeri.ContainsKey(nomeDaModificare))
+            int codiceCarattere=random.Next(50,255);
+            c=(char)codiceCarattere;
+            if(Char.IsAsciiLetter(c) && !Char.IsAsciiLetterLower(c))
             {
-                dizionarioNumeri[nomeDaModificare]=nuovoNumero;
-                Console.WriteLine("Valore modificato");
+                haMaiuscola=true;
+                Console.WriteLine($"Trovato carattere maiuscola: {c}");
             }
+            else if(Char.IsAsciiLetter(c) && Char.IsAsciiLetterLower(c))
+            {
+                haMinuscola=true;
+                Console.WriteLine($"Trovato carattere minuscola: {c}");
+            }
+            else if(Char.IsAsciiDigit(c))
+            {
+                haNumero=true;
+                Console.WriteLine($"Trovato carattere numero: {c}");
+            }
+            else
+            {
+                haCarattereSpeciale=true;
+                Console.WriteLine($"Trovato carattere speciale, codice: {codiceCarattere} carattere {c}");
+            }
+            
+            Console.WriteLine($"carattere casuale: {c} numero: {codiceCarattere}");
+            
         }
-        else
-        {
-            Console.WriteLine($"Nome {nomeDaModificare} inesistente");
-        }
+        password=password+c;
+        Console.WriteLine($"Password: {password}");
+        counter++;
     }
-    else if(risp=="3")
+    if(haMaiuscola && haMinuscola && haNumero && haCarattereSpeciale)
     {
-        Console.WriteLine("Stampa del dizionario");
-        foreach(var kvp in dizionarioNumeri)
-        {
-            Console.WriteLine($"nome: {kvp.Key}\tnumero: {kvp.Value}");
-        }
-        Console.WriteLine("Quale nome vuoi eliminare?");
-        string nomeDaEliminare=Console.ReadLine();
-        bool risultato=dizionarioNumeri.Remove(nomeDaEliminare);
-        if(risultato)
-            Console.WriteLine("Nome rimosso");
-        else
-            Console.WriteLine("Nome non trovato");
-    }
-    else if(risp=="4")
-    {
-        Console.WriteLine("Stampa del dizionario");
-        foreach(var kvp in dizionarioNumeri)
-        {
-            Console.WriteLine($"nome: {kvp.Key}\tnumero: {kvp.Value}");
-        }
+        passwordValida=true;
+        Console.WriteLine($"Password generata: {password}. {haMaiuscola} {haMinuscola} {haNumero} {haCarattereSpeciale} Lunghezza password {password.Length}");
     }
     else
-        break;
-
+    {
+        Console.WriteLine($"Password non valida. Generazione di un'altra password di {numeroCaratteri} caratteri. {haMaiuscola} {haMinuscola} {haNumero} {haCarattereSpeciale}");
+    }
 }
-
-
