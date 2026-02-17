a4871688c2c9baab38dba0019933a956b5fb958e
Marco <marcomarco85@hotmail.it>
2026-02-17 09:27:57 +0100

    Esercitazione slug

--- Esercitazione-05/Program.cs
+++ Esercitazione-05/Program.cs
@@ -1,33 +1,50 @@
 ﻿/*
-Programma che
-- legge un elenco di email, maschera la parte dell' utente lasciando visibile la prima e
-l'ultima lettera mantenendo visibile il dominio (es a*****o@gmail.com) e le stampa
+Legge un elenci di frasi da una lista, per ognuna genera uno slug (versione della frase 
+adatta ad essere usata in un url) e le stampa. Regole dello slug
+- deve essere tutto in minuscolo
+- sostituire gli spazi con i trattini
+- deve rimuovere la punteggiatura
+- deve rimuovere eventuali trattini doppi
 */
 
+List<string> listaFrasi=new List<string>();
+List<string> listaFrasiModificate=new List<string>();
+char[] punteggiatura={',','.',';',':','!','?'};
+listaFrasi.Add("Questa è--, -- una frase..");
+listaFrasi.Add("Seconda frase, per l'esercitazione");
 
-
-List<string> listaEmail=new List<string> {"marco@gmail.com","francesco@hotmail.it","matteo@gmail.it"};
-
-
-foreach(string mail in listaEmail)
+int index=0;
+foreach(string frase in listaFrasi)
 {
-    int i=0;
-    string nuovaMail="";
-    bool modify=true;
-    Console.WriteLine($"Stringa da modificare: {mail}");
-    while(i<mail.Length)
+    string fraseModificata=frase.ToLower();
+    Console.WriteLine($"Frase modificata toLower\n{fraseModificata}");
+    fraseModificata=fraseModificata.Replace(' ','-');
+    Console.WriteLine($"Frase modificata replace spazio\n{fraseModificata}");
+    for(int i=0;i<punteggiatura.Length;i++)
     {
-        char nextChar='a';
-        if(i+1<mail.Length)
-            nextChar=mail[i+1];
-        if(i>0 && modify==true && nextChar!='@')
-            nuovaMail+="*";
-        else
-            nuovaMail+=mail[i];
-        i++;
-        if(i<mail.Length)
-            if(mail[i]=='@')
-                modify=false;
+        Console.WriteLine($"Ricerca segno punteggiatura per carattere {punteggiatura[i]} i: {i}");
+        while(fraseModificata.IndexOf(punteggiatura[i])!=-1)
+        {
+            Console.WriteLine($"trovato carattere da eliminare all'indice {fraseModificata.IndexOf(punteggiatura[i]) } frase: {fraseModificata} i: {i}");
+            fraseModificata=fraseModificata.Remove(fraseModificata.IndexOf(punteggiatura[i]),1);
+            Console.WriteLine($"carattere eliminato, frase: {fraseModificata} i: {i}");
+            
+        } 
+    }          
+    Console.WriteLine($"Frase modificata con punteggiatura\n{fraseModificata}");
+    
+    for(int i=0;i<fraseModificata.Length-1;i++)
+    {
+        
+        if(fraseModificata[i]=='-' && fraseModificata[i+1]=='-')
+        {
+            fraseModificata=fraseModificata.Remove(i,1);
+            //Console.WriteLine($"rimosso carattere {i} frase: {fraseModificata}");
+            i--;
+        }
     }
-    Console.WriteLine($"Stringa modificata: {nuovaMail}");
+    
+    Console.WriteLine($"Frase finale: {fraseModificata}");
 }
+
+
