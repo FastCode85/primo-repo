31cc1e9d1bb31030db910c69c71ffb78f7d3c631
Marco <marcomarco85@hotmail.it>
2026-02-18 11:48:47 +0100

    Esercitazione funzioni 1

--- Esercitazione-05/Program.cs
+++ Esercitazione-05/Program.cs
@@ -1,30 +1,29 @@
 ﻿/*
- Una funzione che unisce due liste di stringhe e restituisce una nuova lista con tutti gli elementi
- un'altra funzione che stampa la lista nuova
+
+Creare una funzione tipo String che si comporta come un ReadLine avanzato che implementa la validazione dell'input:
+- trim: rimuove gli spazi all'inizio e alla fine
+- IsNullOrEmpty: verifica se la stringa è null o vuota
+- ToLower: converte la stringa in minuscolo
+
 */
 
-List<string> lista1=new List<string> {"valore1","valore2","valore3"};
-List<string> lista2=new List<string> {"valore4","valore5","valore6"};
+Console.WriteLine(ReadLineAvanzato());
 
-
-StampaLista(ListaUnita(new List<string>(lista1),lista2));
-StampaLista(lista1);
-Console.WriteLine($"{StampaLista}");
-
-List<string> ListaUnita(List<string> list1, List<string> list2)
+string ReadLineAvanzato()
 {
-    list1.AddRange(list2);
-    return list1;
-}
-
-void StampaLista(List<string> lista)
-{
-    Console.WriteLine("Stampa della lista");
-    foreach(string s in lista)
+    bool valido=false;
+    string s="";
+    while(!valido)
     {
+        Console.WriteLine("Inserisci una frase");
+        s=Console.ReadLine();
+        s=s.Trim();
         Console.WriteLine(s);
+        valido=string.IsNullOrEmpty(s);
+        if(valido)
+            Console.WriteLine("La stringa è null o vuota");
     }
+    return s.ToLower();
 }
-
 
 
