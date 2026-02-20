7bfa736c0e9391df9e0d848838df164ffc46653a
Marco <marcomarco85@hotmail.it>
2026-02-20 13:02:03 +0100

    Esercitazione rubrica avanzata

--- Esercitazione-05/Program.cs
+++ Esercitazione-05/Program.cs
@@ -4,14 +4,27 @@
 
 */
 
-Dictionary<string, string> rubrica = new Dictionary<string, string>()
+using System.Data.Common;
+
+
+
+
+/*
+
+in questa versione del programma, invece di associare un nome a un numero di telefono, associamo ad un id univoco
+generato con il random un dizionario che contiene dizionari con il nome, il nomero di telefono, l' indirizzo, 
+lo stato attivo disattivo. Deve esserci la possibilità di visualizzare la rubrica intera o solo i numeri attivi
+
+*/
+Dictionary<int,Dictionary<string,string>> rubricaAvanzata=new Dictionary<int,Dictionary<string,string>>
 {
-    { "Nome 1", "1234567890" },
-    { "Nome 2", "0987654321" },
-    { "Nome 3", "5555555555" }
+    {1,new Dictionary<string,string> { {"nome","Nome 1"},{"numero","1234567890"},{"indirizzo","Via Roma 1"},{"stato","true"}} },
+    {2,new Dictionary<string,string> { {"nome","Nome 2"},{"numero","0987654321"},{"indirizzo","Via Milano 2"},{"stato","true"}} },
+    {3,new Dictionary<string,string> { {"nome","Nome 3"},{"numero","1597531221"},{"indirizzo","Via Napoli 3"},{"stato","false"}} }
 };
 
 // gestione generale del programma
+Random random=new Random();
 bool continua = true; // variabile per controllare se continuare o meno il programma
 
 
@@ -24,96 +37,142 @@ while (continua)
     {
         case "1":
             // codice per aggiungere un contatto
-            AggiungiContatto(rubrica);
+            AggiungiContatto(rubricaAvanzata,random);
             break;
         case "2":
             // codice per modificare un contatto
-            ModificaContatto(rubrica);
+            ModificaContatto(rubricaAvanzata);
             break;
         case "3":
             // codice per eliminare un contatto
-            EliminaContatto(rubrica);
+            EliminaContatto(rubricaAvanzata);
             break;
         case "4":
             // codice per visualizzare la rubrica
-            StampaRubrica(rubrica);
+            StampaRubricaAvanzata(rubricaAvanzata,true);
             break;
         case "5":
             continua = false; // esce dal programma
             break;
         default:
-            5
+            Console.WriteLine("Input non valido");
             break;
     }
 }
 
-void EliminaContatto(Dictionary<string,string> rubrica)
+void EliminaContatto(Dictionary<int,Dictionary<string,string>> rubricaAvanzata)
 {
-    StampaRubrica(rubrica);
-    string nomeElimina=LeggiInput("Inserisci il nome del contatto da eliminare:");
-    if (rubrica.ContainsKey(nomeElimina))
+    StampaRubricaAvanzata(rubricaAvanzata,false);
+    string input=LeggiInput("Inserisci l'ID da rimuovere:");
+    bool isNumeric=int.TryParse(input, out int idDaRimuovere);
+    if(isNumeric)
     {
-        rubrica.Remove(nomeElimina);
-        Console.WriteLine("Contatto eliminato.");
+        if (rubricaAvanzata.ContainsKey(idDaRimuovere))
+        {
+            rubricaAvanzata.Remove(idDaRimuovere);
+            Console.WriteLine("Contatto eliminato.");
+        }
+        else
+        {
+            Console.WriteLine("Contatto non trovato.");
+        }
     }
     else
-    {
-        Console.WriteLine("Contatto non trovato.");
-    }
+        Console.WriteLine("L'ID inserito non è un numero");
 }
 
 void StampaRubrica(Dictionary<string,string> rubrica)
 {
     Console.WriteLine("Rubrica:");
+
     foreach (var kvp in rubrica)
     {
         Console.WriteLine($"Nome: {kvp.Key}\tNumero: {kvp.Value}");
     }
 }
-void ModificaContatto(Dictionary<string,string> rubrica)
+void ModificaContatto(Dictionary<int,Dictionary<string,string>> rubricaAvanzata)
 {
-    StampaRubrica(rubrica);
-    string nomeModifica=LeggiInput("Inserisci il nome del contatto da modificare:");
-    if (rubrica.ContainsKey(nomeModifica))
+    StampaRubricaAvanzata(rubricaAvanzata,false);
+    string input=LeggiInput("Inserisci l'ID del contatto da modificare:");
+    bool isNumero=int.TryParse(input, out int idInserito);
+    if(isNumero)
     {
-        Console.WriteLine("Inserisci il nuovo numero di telefono:");
-        string nuovoNumero = Console.ReadLine().Trim();
-        rubrica[nomeModifica] = nuovoNumero;
-        Console.WriteLine("Contatto modificato.");
+        if (rubricaAvanzata.ContainsKey(idInserito))
+        {
+
+            string nome=LeggiInput("Inserisci il nome del contatto:");
+            string numero = LeggiInput("Inserisci il numero di telefono del contatto:");
+            string indirizzo=LeggiInput("Inserisci l'indirizzo del contatto:");
+            string stato=LeggiStato();
+            Dictionary<string,string> singoloContatto=rubricaAvanzata[idInserito];
+            singoloContatto["nome"] = nome;
+            singoloContatto["numero"] = numero;
+            singoloContatto["indirizzo"] = indirizzo;
+            singoloContatto["stato"] = stato;
+            Console.WriteLine($"Contatto modificato. nome: {singoloContatto["nome"]} numero: {singoloContatto["numero"]}");
+        }
+        else
+        {
+            Console.WriteLine($"ID non trovato. ID: {idInserito}");
+        }
     }
     else
-    {
-        Console.WriteLine("Contatto non trovato.");
-    }
+        Console.WriteLine("L'ID inserito non è numerico");
 }
 
 string LeggiInput(string messaggio)
 {
     Console.WriteLine(messaggio);
     string input = Console.ReadLine().Trim();
-    Console.Clear();
+    //Console.Clear();
     return input;
 }
 
-void AggiungiContatto(Dictionary<string,string> rubrica)
+string LeggiStato()
 {
+    string input=LeggiInput("Inserisci lo stato del contatto (attivo,non attivo):");
+    Console.WriteLine($"stampa stato {input} stato valido: {StatoValido(input)}");
+    while(!StatoValido(input))
+    {
+    Console.WriteLine($"2stampa stato {input} stato valido: {StatoValido(input)}");
+    input=LeggiInput($"stato non valido, Inserisci lo stato del contatto (attivo,non attivo): {input}");
+    }
+    return input;
+}
+
+void AggiungiContatto(Dictionary<int,Dictionary<string,string>> rubricaAvanzata,Random random)
+{
+    int randomId=random.Next(1,1000);
+    while(rubricaAvanzata.ContainsKey(randomId))
+        randomId=random.Next(1,1000);
     string nome=LeggiInput("Inserisci il nome del contatto:");
     string numero = LeggiInput("Inserisci il numero di telefono del contatto:");
+    string indirizzo=LeggiInput("Inserisci l'indirizzo del contatto:");
+    string stato=LeggiStato();
 
-    if (rubrica.ContainsKey(nome))
-    {
-        string risposta=LeggiInput("Il contatto esiste già. Vuoi aggiornare il numero di telefono? (s/n)");
-        if (risposta == "s")
-        {
-            rubrica[nome] = numero; // aggiorna il numero di telefono del contatto esistente
-            Console.WriteLine("Numero di telefono aggiornato.");
-        }
-    }
+    Dictionary<string,string> dettagli=new Dictionary<string,string>();
+    dettagli.Add("nome",nome);
+    dettagli.Add("numero",numero);
+    dettagli.Add("indirizzo",indirizzo);
+    dettagli.Add("stato",stato);
+    rubricaAvanzata.Add(randomId,dettagli);
+}
+
+bool StatoValido(string stato)
+{
+    if(stato=="true" || stato=="false")
+        return true;
     else
-    {
-        rubrica.Add(nome, numero);
-        Console.WriteLine("Contatto aggiunto.");
-    }
+        return false;
+}
+
+bool StatoAttivo(string stato)
+{
+    bool parseOk=bool.TryParse(stato, out bool value);
+    if(parseOk)
+        return value;
+    else 
+        return false;
 }
 
 void StampaMenu()
@@ -122,6 +181,21 @@ void StampaMenu()
     Console.WriteLine("1. Aggiungi un contatto");
     Console.WriteLine("2. Modifica un contatto");
     Console.WriteLine("3. Elimina un contatto");
-    Console.WriteLine("4. Visualizza la rubrica");
+    Console.WriteLine("4. Visualizza la rubrica (tutti)");
     Console.WriteLine("5. Esci");
+}
+
+void StampaRubricaAvanzata(Dictionary<int,Dictionary<string,string>> rubricaAvanzata, bool soloAttivi)
+{
+    Console.WriteLine($"Stampa rubrica avanzata, solo numeri attivi={soloAttivi}");
+    foreach(var kvp in rubricaAvanzata)
+    {
+        bool stampa=false;
+        if(!soloAttivi)
+            stampa=true;
+        else if(StatoAttivo(kvp.Value["stato"]))
+            stampa=true;
+        if(stampa)
+            Console.WriteLine($"ID: {kvp.Key,-5} Nome: {kvp.Value["nome"],-20} Numero: {kvp.Value["numero"],20} Indirizzo: {kvp.Value["indirizzo"],20} Stato: {kvp.Value["stato"],10}");
+    }
 }
